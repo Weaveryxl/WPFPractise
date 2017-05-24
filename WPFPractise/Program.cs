a494a4004c11f2d5e975401fa5837fe943038b85
Weaveryxl <weaveryxl549@gmail.com>
2017-05-24 09:43:44 -0400

    create app using only code, not xaml

---
+++ WPFPractise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WPFPractise
+{
+    class Program : Application
+    {
+        [STAThread()]
+
+        static void Main()
+        {
+            Program app = new Program();
+            app.MainWindow = new window1();
+            app.MainWindow.ShowDialog();
+        }
+    }
+}
